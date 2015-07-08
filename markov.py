cc0b2c09373b42a548adcd8b6dbd2b9df1e87c7b
Hackbright Student <info@hackbrightacademy.com>
2015-07-08 12:40:15 -0700

    fitted output text to max tweet length

--- markov.py
+++ markov.py
@@ -1,4 +1,7 @@
 import random
+# from sys import argv
+
+# script, filename, text_length = argv
 
 def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
@@ -19,18 +22,18 @@ def make_chains(corpus):
 
 def make_text(filename):
     """Takes dictionary of markov chains; returns random text."""
-
     # Get a Markov chain
     chain_dict = make_chains(filename)
     chosen_key = random.choice(chain_dict.keys())
     our_string = " ".join(chosen_key)
-    count = 0
-    while count < 40:
+    character_count = 0
+    while character_count < 140:
         for value in chain_dict[chosen_key]:
             value = random.choice(chain_dict[chosen_key])
             our_string = our_string + " " + (value)
             split_string = our_string.split(" ")
             chosen_key = (split_string[-2], split_string[-1])
-        count += 1
+            character_count = len(our_string)
     print our_string
+
 make_text(raw_input("Enter the filename of the selected text: "))
